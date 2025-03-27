c0956c6012d309a78c48f686d4c4593e5c6137ac
peterffydominika <90351361+peterffydominika@users.noreply.github.com>
2025-03-27 14:53:28 +0100

    Update orokbefogadas.sql

--- Kutya_orokbefogadas/orokbefogadas.sql
+++ Kutya_orokbefogadas/orokbefogadas.sql
@@ -10,3 +10,15 @@ CREATE TABLE orokbefogadasok (
     dog VARCHAR(50) NOT NULL,
     adoption_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE regisztraciok (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    sirname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    password1 CHAR(64) NOT NULL,
+    resetpassword CHAR(64) NOT NULL
+);
